c29f8c03ac67e3050c9dd5bf9e63aa4296e2414c
chethan-gen <chaithuregala123@gmailcom>
2025-03-28 13:40:06 +0530

    adding fies

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRhPl_u9LLaxNt0yIt8EIm7KoEUorPTcPuz0g&s" />
+     <h1>Chethan</h1>
+     <h2>regala.chethan.s89@Kalvium.community</h2>
+     <h2>Rajmundry,Andhra pradesh</h2>
+     <h2>https://github.com/chethan-gen</h2>
+    </>
+  )
+}
+
+export default App
